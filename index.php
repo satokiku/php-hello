8edb171b8b7e86728da548a63b024a04fc3958a1
satokiku <satoru_kikuchi@gigaphoton.com>
2019-04-28 18:29:24 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; "<br/>\n"; echo '[SERVER_ADDR] : '.$_SERVER['SERVER_ADDR']."<br/>\n"; ?> 
+ </body>
+</html>
