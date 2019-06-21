d44e299fdb7e3993c6c2f287e5b757832c072bd8
satokiku <satoru_kikuchi@gigaphoton.com>
2019-06-21 10:49:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
  <head>
-  <title>PHP Test2</title>
+  <title>PHP Test3!!</title>
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; "<br/>\n"; echo '[SERVER_ADDR] : '.$_SERVER['SERVER_ADDR']."<br/>\n"; ?> 
